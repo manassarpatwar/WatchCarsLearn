c3d577044ab8900c4732532d1b5f33b6a704489b
Manas Sarpatwar <manas.mahesh.sarpatwar@gmail.com>
2020-03-25 12:31:56 +0530

    Revert "Version 2"

--- boundary.js
+++ boundary.js
@@ -1,60 +1,55 @@
-class Boundary{
-    x1;
-    y1;
-    x2;
-    y2;
-    
-    constructor(x1, y1, x2, y2){
+class Boundary {
+    constructor(x1, y1, x2, y2) {
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
         this.y2 = y2;
     }
 
-    getLength(){
-        return Math.round(dist(this.x1, this.y1, this.x2, this.y2));
+    drawBoundary(color = "255, 255, 255") {
+        drawLine(this.x1, this.y1, this.x2, this.y2, 0.6, 4, color);
     }
-    
+
+    getLength(){
+        return getDist(this.x1, this.y2, this.x1, this.y2);
+    }
+
     setStart(p){
         this.x1 = p.x;
         this.y1 = p.y;
     }
-    
+
     setEnd(p){
         this.x2 = p.x;
         this.y2 = p.y;
     }
 
-    getLeftPoint(point){
-        let a = Math.atan2(this.y2-this.y1, this.x2-this.x1)
-        if(point == "END")
-            return createVector(this.x2+TRACKWIDTH*Math.cos(a-Math.PI/2), this.y2+TRACKWIDTH*Math.sin(a-Math.PI/2))
-        else
-            return createVector(this.x1+TRACKWIDTH*Math.cos(a-Math.PI/2), this.y1+TRACKWIDTH*Math.sin(a-Math.PI/2))
+    static getAngle(boundary1, boundary2){
+        const x1 = boundary1.x1;
+        const x2 = boundary1.x2;
+        const y1 = boundary1.y1;
+        const y2 = boundary1.y2;
+
+        const x3 = boundary2.x1;
+        const x4 = boundary2.x2;
+        const y3 = boundary2.y1;
+        const y4 = boundary2.y2;
+
+        var dAx = x2 - x1;
+        var dAy = y2 - y1;
+        var dBx = x4 - x3;
+        var dBy = y4 - y3;
+        var angle = Math.atan2(dAx * dBy - dAy * dBx, dAx * dBx + dAy * dBy);
+
+        return angle;
     }
 
-    getRightPoint(point){
-        let a = Math.atan2(this.y2-this.y1, this.x2-this.x1)
-        if(point == "END")
-            return createVector(this.x2+TRACKWIDTH*Math.cos(a+Math.PI/2), this.y2+TRACKWIDTH*Math.sin(a+Math.PI/2));
-        else
-            return createVector(this.x1+TRACKWIDTH*Math.cos(a+Math.PI/2), this.y1+TRACKWIDTH*Math.sin(a+Math.PI/2));
-    }
-    
-    display(){
-        push();
-        stroke(100);
-        strokeWeight(2);
-        line(this.x1, this.y1, this.x2, this.y2); 
-        pop();
-    }
-    
     static getIntersection(boundary1, boundary2){
         const x1 = boundary1.x1;
         const x2 = boundary1.x2;
         const y1 = boundary1.y1;
         const y2 = boundary1.y2;
-        
+
         const x3 = boundary2.x1;
         const x4 = boundary2.x2;
         const y3 = boundary2.y1;
@@ -65,39 +60,22 @@ class Boundary{
         }
         ua = ((x4 - x3)*(y1 - y3) - (y4 - y3)*(x1 - x3))/denom;
         ub = ((x2 - x1)*(y1 - y3) - (y2 - y1)*(x1 - x3))/denom;
-        return createVector(x1 + ua * (x2 - x1), y1 + ua * (y2 - y1));
-        
+        return new Vector(x1 + ua * (x2 - x1), y1 + ua * (y2 - y1));
+
+        // {
+        //     x: x1 + ua * (x2 - x1),
+        //     y: y1 + ua * (y2 - y1),
+        //     seg1: ua >= 0 && ua <= 1,
+        //     seg2: ub >= 0 && ub <= 1
+        // };
     }
 
-    getAngle(){
-        let a = Math.atan2(this.y2 - this.y1, this.x2 - this.x1);
-        return a;
-    }
-
-    static distance(boundary, x0, y0) {
-        const x1 = boundary.x1;
-        const x2 = boundary.x2;
-        const y1 = boundary.y1;
-        const y2 = boundary.y2;
-
-        return Math.round(((Math.abs((y1 - y2) * x0 -
-                         (x1 - x2) * y0 - 
-                         x1*(y1-y2)+y1*(x1-x2))) /
-                (Math.pow((Math.pow(y2 - y1, 2) + 
-                           Math.pow(x1 - x2, 2)), 
-                          0.5))));
-    }
-
-    static getAreaWithPoint(boundary, x, y){
-        return (1/2)*boundary.getLength()*Boundary.distance(boundary, x, y);
-    }
-    
     static intersects(boundary1, boundary2) {
         const a = boundary1.x1;
         const b = boundary1.x2;
         const c = boundary1.y1;
         const d = boundary1.y2;
-        
+
         const p = boundary2.x1;
         const q = boundary2.x2;
         const r = boundary2.y1;
@@ -105,44 +83,26 @@ class Boundary{
         var det, gamma, lambda;
         det = (c - a) * (s - q) - (r - p) * (d - b);
         if (det === 0) {
-            return false;
+          return false;
         } else {
-            lambda = ((s - q) * (r - a) + (p - r) * (s - b)) / det;
-            gamma = ((b - d) * (r - a) + (c - a) * (s - b)) / det;
-            return (0 < lambda && lambda < 1) && (0 < gamma && gamma < 1);
+          lambda = ((s - q) * (r - a) + (p - r) * (s - b)) / det;
+          gamma = ((b - d) * (r - a) + (c - a) * (s - b)) / det;
+          return (0 < lambda && lambda < 1) && (0 < gamma && gamma < 1);
         }
     };
-    
-    static overlaps(boundary1, boundary2){
-        const p0_x = boundary1.x1;
-        const p0_y = boundary1.y1;
-        const p1_x = boundary1.x2;
-        const p1_y = boundary1.y2;
-        const p2_x = boundary2.x1;
-        const p2_y = boundary2.y1;
-        const p3_x = boundary2.x2;
-        const p3_y = boundary2.y2;
-        
-        
-        
-        var s1_x, s1_y, s2_x, s2_y;
-        s1_x = p1_x - p0_x;
-        s1_y = p1_y - p0_y;
-        s2_x = p3_x - p2_x;
-        s2_y = p3_y - p2_y;
-        
-        var s, t;
-        s = (-s1_y * (p0_x - p2_x) + s1_x * (p0_y - p2_y)) / (-s2_x * s1_y + s1_x * s2_y);
-        t = ( s2_x * (p0_y - p2_y) - s2_y * (p0_x - p2_x)) / (-s2_x * s1_y + s1_x * s2_y);
-        
-        let m1 = s1_y/s1_x; 
-        let m2 = s2_y/s2_x;
-        
-        //If they have the same slope check for the points to intersect
-        if(m1 == m2)
-        return ((p0_x - p2_x)*(p0_y - p3_y) - (p0_x - p3_x)*(p0_y - p2_y) == 0 || (p1_x - p2_x)*(p1_y - p3_y) - (p1_x - p3_x)*(p1_y - p2_y) == 0);
-        
-        return (s >= 0 && s <= 1 && t >= 0 && t <= 1);
+
+    static distance(boundary, x0, y0) {
+        const x1 = boundary.x1;
+        const x2 = boundary.x2;
+        const y1 = boundary.y1;
+        const y2 = boundary.y2;
+
+        return ((Math.abs((y2 - y1) * x0 - 
+                         (x2 - x1) * y0 + 
+                         x2 * y1 - 
+                         y2 * y1)) /
+                (Math.pow((Math.pow(y2 - y1, 2) + 
+                           Math.pow(x2 - x1, 2)), 
+                          0.5)));
     }
-    
 }
--- ray.js
+++ ray.js
@@ -1,22 +1,21 @@
-class Ray{
+function Vector(x, y) {
+    this.x = x;
+    this.y = y;
+}
 
-    constructor(tail, heading, length){
-        this.tail = tail;
-        this.heading = heading;
-        this.length = length;
-        this.maxlength = length;
+class Ray {
+    distance = 0;
+    constructor(x, y, angle) {
+        this.x = x;
+        this.y = y;
+        this.angle = angle;
+        this.vector = new Vector(this.x + Math.cos(this.angle), this.y + Math.sin(this.angle));
     }
 
-    setLength(l){
-        this.length = l
-    }
-
-    resetLength(){
-        this.length = this.maxlength;
-    }
-
-    getPoint2(len = this.length){
-        return createVector(this.tail.x+len*Math.cos(this.heading), this.tail.y+len*Math.sin(this.heading));
+    setAngle(a) {
+        this.angle = a;
+        this.vector.x = this.x + Math.cos(this.angle);
+        this.vector.y = this.y + Math.sin(this.angle);
     }
 
     isHitting(boundary) {
@@ -24,25 +23,26 @@ class Ray{
         const x2 = boundary.x2;
         const y1 = boundary.y1;
         const y2 = boundary.y2;
-        
-        let point2 = this.getPoint2(this.maxlength)
-        const x3 = this.tail.x;
-        const x4 = point2.x;
-        const y3 = this.tail.y;
-        const y4 = point2.y;
-        
+
+        const x3 = this.x;
+        const x4 = this.vector.x;
+        const y3 = this.y;
+        const y4 = this.vector.y;
+
         const den = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
         if (den == 0)
-        return;
+            return;
         const t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / den;
         const u = -((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / den;
         //    console.log(t);
         if (t >= 0 && t <= 1 && u > 0) {
             const x = x1 + t * (x2 - x1);
             const y = y1 + t * (y2 - y1);
-            return createVector(x, y);
+            return new Vector(x, y);
         } else {
             return false;
         }
+
     }
+
 }
